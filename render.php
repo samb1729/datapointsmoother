ff250d5e0457383d2f8aaa61abd1e5d7b58176c0
Sam Broughton <sam@26th-zerk.co.uk>
2014-06-19 11:24:35 +0100

    Move deserialisation to render.php

--- render.php
+++ render.php
@@ -1,4 +1,14 @@
 <?php
 require("smoother.php");
 
+$serialized = trim(file_get_contents("dreyri"));
+$updates = unserialize($serialized);
+
+var_dump(smooth_skill($updates, 0, 86400, last_update($updates)));
 ?>
+
+<html>
+  <head>
+    <title>Smoother output</title>
+  </head>
+</html>
--- smoother.php
+++ smoother.php
@@ -1,6 +1,27 @@
 <?php
 include("stats.php");
 
-$serialized = trim(file_get_contents("dreyri"));
-$updates = unserialize($serialized);
+function last_update($updates) {
+    return $updates[count($updates) - 1]->time;
+}
+
+function smooth_skill($updates, $skill_id, $time_span, $end_time,
+                      $threshold = 25200, $limit = 400) {
+    $start_time = $end_time - $time_span;
+    $trimmed_updates = array();
+
+    foreach($updates as $update) {
+        if ($start_time <= $update->time && $update->time <= $end_time)
+            $trimmed_updates[] = $update;
+    }
+
+    $update_count = count($trimmed_updates);
+    $filtered_updates = array();
+
+    for($i = 0; $i < $update_count; $i++) {
+
+    }
+
+    return $trimmed_updates;
+}
 ?>
