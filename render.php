42972d4e87d1d13e04bd7856fb058334e53357dc
Sam Broughton <sam@26th-zerk.co.uk>
2014-06-19 11:43:25 +0100

    Stop using var_dump

--- render.php
+++ render.php
@@ -4,7 +4,7 @@ require("smoother.php");
 $serialized = trim(file_get_contents("dreyri"));
 $updates = unserialize($serialized);
 
-var_dump(smooth_skill($updates, 0, 86400, last_update($updates)));
+$updates = smooth_skill($updates, 0, 86400, last_update($updates));
 ?>
 
 <html>
